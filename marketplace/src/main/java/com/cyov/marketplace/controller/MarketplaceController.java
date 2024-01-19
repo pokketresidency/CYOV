5720401d64b48da15803b07b441a245f0055f038
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-20 00:40:14 +0530

    file structure setup

--- marketplace/src/main/java/com/cyov/marketplace/controller/MarketplaceController.java
+++ marketplace/src/main/java/com/cyov/marketplace/controller/MarketplaceController.java
@@ -1,5 +1,27 @@
 package com.cyov.marketplace.controller;
 
+import com.cyov.marketplace.model.entity.SampleEntity;
+import com.cyov.marketplace.service.SampleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/books")
 public class MarketplaceController {
+    private final SampleService bookService;
+
+    @Autowired
+    public MarketplaceController(SampleService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<SampleEntity> getAllBooks() {
+        return bookService.findAllBooks();
+    }
 
 }
+
---
+++ marketplace/src/main/java/com/cyov/marketplace/model/entity/SampleEntity.java
@@ -0,0 +1,18 @@
+package com.cyov.marketplace.model.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class SampleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+
+    // Getters and setters
+}
