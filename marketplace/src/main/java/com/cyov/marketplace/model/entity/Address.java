f35135e3717d681af3b99ad0c19c5d5b12462994
Rahul Mishra <rahul.pm259.rm29@gmail.com>
2024-01-20 01:32:58 +0530

    :sparkles: added entity

---
+++ marketplace/src/main/java/com/cyov/marketplace/model/entity/Address.java
@@ -0,0 +1,40 @@
+package com.cyov.marketplace.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    private Boolean isPrimary;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Getters and Setters
+}
