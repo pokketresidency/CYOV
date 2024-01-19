905c36ef51d80defc1a970ba551661ddeff1401b
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-20 01:00:59 +0530

    complete api

--- marketplace/src/main/java/com/cyov/marketplace/model/entity/SampleEntity.java
+++ marketplace/src/main/java/com/cyov/marketplace/model/entity/SampleEntity.java
@@ -1,10 +1,12 @@
 package com.cyov.marketplace.model.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
 public class SampleEntity {
@@ -14,5 +16,36 @@ public class SampleEntity {
     private String title;
     private String author;
 
-    // Getters and setters
+    public SampleEntity() {
+    }
+
+    public SampleEntity(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
