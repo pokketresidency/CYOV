d476c32a7a53c3198bd5274c641c492acaead104
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-01-20 13:40:01 +0530

    Added Kafka

---
+++ marketplace/src/main/java/com/cyov/marketplace/controller/KafkaController.java
@@ -0,0 +1,19 @@
+package com.cyov.marketplace.controller;
+
+import com.cyov.marketplace.kafka.publisher.KafkaPublisherService;
+import com.cyov.marketplace.model.dto.CommunicationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    @Autowired
+    private KafkaPublisherService producerService;
+
+    @PostMapping("/publish/{topic}")
+    public void sendMessageToKafkaTopic(@RequestBody CommunicationDTO communicationDTO, @PathVariable("topic") String topic) {
+        producerService.sendMessageToCommunicationTopic(topic, communicationDTO);
+    }
+}
---
+++ marketplace/src/main/java/com/cyov/marketplace/kafka/consumer/PartitionedTopicConsumer.java
@@ -0,0 +1,50 @@
+package com.cyov.marketplace.kafka.consumer;
+
+import com.cyov.marketplace.model.dto.CommunicationDTO;
+import com.cyov.marketplace.utils.PublishToDLQ;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+
+
+@Component
+@Slf4j
+@KafkaListener(topics = "${spring.kafka.communication-partitioned-topic}", groupId = "${spring.kafka.communication-partitioned-group-id}", concurrency = "15")
+public class PartitionedTopicConsumer {
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
+
+
+    @Autowired
+    private PublishToDLQ dlqPublisherUtil;
+
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @KafkaHandler
+    public void handleEvent(CommunicationDTO event) throws JsonProcessingException {
+        try {
+
+            log.info("Message received - {}", event);
+
+        } catch (HttpServerErrorException exception) {
+            log.error("Error occurred :  Event :  {}  Clevertap threw : ", event, exception);
+            throw new HttpServerErrorException(exception.getStatusCode());
+        } catch(Exception ex) {
+            log.error("Error occurred :  Event :  {} , Pushing to DLQ Exception : ", event, ex);
+            dlqPublisherUtil.publishEventToDlq(event, objectMapper.writeValueAsString(event),
+                    ex.getMessage());
+        }
+    }
+}
+
